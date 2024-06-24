67af851ceafbb20e52e28bcf20dbc79e2eb70a8f
Judel Fintch <judfintch@gmail.com>
2024-06-24 14:55:24 +0200

    feat :remove files connect to login pages

--- index.php
+++ index.php
@@ -3,8 +3,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<?php require_once('bdd_app_gst_connect/allscirpt.inc.php');?>
-<title><?php echo $entete?></title>
+
 <link rel="shortcut" href="lgo.png" >
 
 <?php  include('bdd_app_gst_connect/class_authetification.php');?>
